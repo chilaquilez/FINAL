6decccb9daf6157c71ae9321445185b4b784afae
chilaquilez <mendotavo8@hotmail.com>
2025-12-09 22:26:06 +0000

    c

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const name = document.querySelector("#name");
-const  = document.querySelector("#name");
-const name = document.querySelector("#name");
+const paragraph = document.querySelector("#paragraph");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function(){
+    const userName = nameInput.value;
+
+if (userName) {
+    paragraph.textContent = "Greetings " + userName + "!";
+} else {
+    greetingMessage.textcontent = "PLEASE ENTER YOUR NAME.";
+});
